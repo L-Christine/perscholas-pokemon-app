fe8234902f8d1cb79fe79e138267039f1e067b9f
Christine Lee <59518411+christineslee@users.noreply.github.com>
2022-10-08 22:48:07 -0400

    third commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const pokemon = require('./models/pokemon');
 
 //====App init
 const app = express()
@@ -12,4 +13,8 @@ app.listen(PORT, () => {
 //====Routes
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
+})
+
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon)
 })
