bfe86607eac1c541a2065880a7c66d0b2c3aa85b
Christine Lee <59518411+christineslee@users.noreply.github.com>
2022-10-11 13:42:13 -0400

    nineth commit -MongoDB

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.engine('jsx', require ('express-react-views').createEngine())
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 
-    mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     
     mongoose.connection.once('open', ()=> {
     console.log('connected to mongo');
